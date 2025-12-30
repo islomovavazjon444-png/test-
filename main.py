48f1712a0fb1adef711f533fad23393a31df92a5
Avazjon Islomov <islomovavazjon444@gmail.com>
2025-12-30 15:33:13 +0500

    write some code

--- main.py
+++ main.py
@@ -394,7 +394,7 @@
 
 
 # accessing keys and values in a dict 
-y = {'pork': 25.3, 'beef': 33.8, 'chicken': 22.7}
+# y = {'pork': 25.3, 'beef': 33.8, 'chicken': 22.7}
 # print(y.keys())
 # print(y.values())
 # print(y.items())   #key-value pairs 
@@ -417,3 +417,43 @@ y = {'pork': 25.3, 'beef': 33.8, 'chicken': 22.7}
 
 
 # List Comperhensions
+
+# get values within a range 
+# under_10 = [x for x in range(10)]
+# print('under_10: ' + str(under_10))
+
+# # get squared values 
+# squares = [x**2 for x in under_10]
+# print('squares: ' + str(squares))
+
+# # get odd and even numbers using mod
+# odds = [x for x in range(10) if x%2 == 1]
+# print('odds: ' + str(odds))
+# even = [x for x in range(10) if x%2 == 0 ]
+# print('even: ' + str(even) ) 
+
+
+# get multiples of 10
+
+# ten = [x  * 10 for x in range (10)]
+# print('ten_x: ' + str(ten))
+
+# # get all numbers from a string 
+# s = 'I love 2 go t0 the store 7 times a w3ek'
+# nums = [x for x in s if x.isnumeric()]
+# print('nums: ' + '' .join(nums))
+
+# # get index of a list item
+# names = ['Cosmo', 'Pedro', 'Anu', 'Ray']
+# idx =[k for k, v in enumerate(names) if v == 'Pedro']
+# print('index = ' + str(idx[0]))
+
+
+# # delete an item from a list 
+
+# letters = [x for x in 'ABCDEF']
+# letrs = [a for a in letters if a != 'C']
+# print(letters, letrs)
+
+
+
